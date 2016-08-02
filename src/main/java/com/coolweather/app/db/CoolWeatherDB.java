0b6fbcd1e1cf847aa553ab4ddbbce8b157751e09
yinbi <1160118373@qq.com>
2016-08-02 11:02:34 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/main/java/com/coolweather/app/db/CoolWeatherDB.java
@@ -0,0 +1,152 @@
+package com.coolweather.app.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/8/2 0002.
+ */
+public class CoolWeatherDB {
+    /*
+    数据库的名字
+     */
+    public static final String DB_NAME="cool_weather";
+    /*
+    数据库的版本
+     */
+    public static final int VERSION=1;
+    private static CoolWeatherDB coolWeatherDB;
+    private SQLiteDatabase db;
+    private CoolWeatherDB(Context context){
+        CoolWeatherOpenHelper dbHelper=new CoolWeatherOpenHelper(context,DB_NAME,null,VERSION);
+        db=dbHelper.getWritableDatabase();
+    }
+
+    /**
+     * @param context
+     * @return 获取CoolWeatherDB的实例
+     */
+    public synchronized static CoolWeatherDB getInstance(Context context){
+        if (coolWeatherDB==null){
+            coolWeatherDB=new CoolWeatherDB(context);
+        }
+        return coolWeatherDB;
+    }
+
+    /**
+     * @param province 将Province实例存储到数据库
+     */
+    public void saveProvince(Province province){
+        if (province==null){
+            ContentValues values=new ContentValues();
+            values.put("province_name",province.getProvinceName());
+            values.put("province_code",province.getProvinceCode());
+            db.insert("Province",null,values);
+        }
+    }
+
+    /**
+     * @return 从数据库读取全国的省份信息
+     */
+    public List<Province> LoadProvinces(){
+        List<Province> list=new ArrayList<Province>();
+        Cursor cursor=db.query("Province",null,null,null,null,null,null);
+        if (cursor.moveToFirst()){
+            do {
+                Province province=new Province();
+                province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+                province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+                list.add(province);
+            }while (cursor.moveToNext());
+        }
+        if (cursor!=null){
+            cursor.close();
+        }
+        return list;
+    }
+
+    /**
+     * @param city 将City的实例存储到数据库
+     */
+    public void saveCity(City city){
+        if (city!=null){
+            ContentValues values =new ContentValues();
+            values.put("city_name",city.getCityName());
+            values.put("city_code",city.getCityCode());
+            values.put("province_id",city.getProvinceId());
+            db.insert("City",null,values);
+        }
+    }
+
+    /**
+     * @param provinceId  丛数据库中读取某一个省下面的所有的城市信息
+     * @return
+     */
+    public List<City> loadCities(int provinceId){
+        List<City> list =new ArrayList<City>();
+        Cursor cursor = db.query("City", null, "province_id=?", new String[]{String.valueOf(provinceId)}, null, null, null);
+        if (cursor.moveToFirst()){
+            do {
+                City city=new City();
+                city.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+                city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+                city.setProvinceId(provinceId);
+                list.add(city);
+            }while (cursor.moveToNext());
+        }
+        if (cursor!=null){
+            cursor.close();
+        }
+        return list;
+    }
+
+
+    /**
+     * @param county  将County实例存储到数据库
+     */
+    public void saveCounty(County county){
+        if (county!=null){
+            ContentValues values=new ContentValues();
+            values.put("county_name",county.getCountyName());
+            values.put("county_code",county.getCountyCode());
+            values.put("county_id",county.getCityId());
+            db.insert("County",null,values);
+        }
+    }
+
+    /**
+     * @param cityId  从数据库读取某城市下的所有的县信息
+     * @return
+     */
+    public List<County> loadCounties(int cityId){
+        List<County> list =new ArrayList<County>();
+        Cursor cursor=db.query("County",null,"city_id=?",new String[]{String.valueOf(cityId)},null,null,null);
+        if (cursor.moveToFirst()){
+            do {
+                County county=new County();
+                county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                county.setCityId(cityId);
+                county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
+                county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
+                list.add(county);
+            }while (cursor.moveToNext());
+        }
+        if (cursor!=null){
+            cursor.close();
+        }
+        return list;
+    }
+
+}
---
+++ src/main/java/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,26 @@
+package com.coolweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2016/8/2 0002.
+ */
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+    public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table Province(id integer primary key autoincrement,province_name text,province_code text)");
+        db.execSQL("create table City(id integer primary key autoincrement,city_name text,city_code text,province_id integer)");
+        db.execSQL("create table City(id integer primary key autoincrement,county_name text,county_code text,city_id integer)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
